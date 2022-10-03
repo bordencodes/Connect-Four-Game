b233823fa2ed9b354c9df200927b13b324e1caef
Bret Borden <bordenb21@gmail.com>
2022-10-03 14:20:26 -0400

    created playGame html for landing page. Updated js with global variables, anticipated functions, and event listeners

---
+++ script.js
@@ -0,0 +1,35 @@
+//Global variables
+let player1 = 0
+let player2 = 0
+let score1 = document.querySelector('#score1')
+let score2 = document.querySelector('#score2')
+let scoreDisplay = document.querySelector('.score-board')
+let currentPlayer = 'Player 1'
+let gameOver = false
+const gameAlerts = document.querySelector('.gameAlerts')
+const playGame = document.querySelector('.playGame')
+const plagyAgain = document.querySelector('.playAgain')
+const resetGame = document.querySelector('.resetGame')
+
+//Functions for game logic
+
+function playGame() {}
+
+function clickSpace() {}
+
+function updateSpace() {}
+
+function switchPlayer() {}
+
+function checkForWinner() {}
+
+function updateScore() {}
+
+function playAgain() {}
+
+function resetGame() {}
+
+//Evemt listeners
+playGame.addEventListener('click', playGame)
+playAgain.addEventListener('click', playAgain)
+resetGame.addEventListener('click', resetGame)
