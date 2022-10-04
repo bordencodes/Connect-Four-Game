304fd16588cd070157b8d5940097e5b723dd3b40
Bret Borden <bordenb21@gmail.com>
2022-10-04 15:46:51 -0400

    made adjustments to html file, and updated functions in js

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ let player2 = 0
 let score1 = document.querySelector('#score1')
 let score2 = document.querySelector('#score2')
 let scoreDisplay = document.querySelector('.score-board')
+let p1 = document.getElementById('p1')
+let p2 = document.getElementById('p2')
 let currentPlayer = 1
-let gameGrid = [
+let gameBoard = [
   ['', '', '', '', '', '', ''],
   ['', '', '', '', '', '', ''],
   ['', '', '', '', '', '', ''],
@@ -13,7 +15,7 @@ let gameGrid = [
   ['', '', '', '', '', '', ''],
   ['', '', '', '', '', '', '']
 ]
-let gameSpace = document.querySelector('.space')
+let gameSpace = document.querySelectorAll('.space')
 let spaceInUse = false
 let gameOver = false
 const gameAlerts = document.querySelector('.gameAlerts')
@@ -23,23 +25,45 @@ const reset = document.querySelector('.resetGame')
 
 //Functions for game logic
 
-function playGame() {
-  for (let i = 0; i < gameSpace.length; i++) {
-    gameSpace[i].addEventListener('click', function () {
-      clickSpace(i)
-    })
-  }
-  gameSpace.forEach(function (gameSpace) {
-    gameAlerts.innerHTML = `It's ${currentPlayer}'s turn!`
-    spaceInUse = true
+// playGame()
+
+for (let i = 0; i < gameSpace.length; i++) {
+  gameSpace[i].addEventListener('click', function () {
+    clickSpace(i)
   })
 }
+gameSpace.forEach(function (gameSpace) {
+  gameAlerts.innerHTML = `It's ${currentPlayer}'s turn!`
+  spaceInUse = true
+})
 
-function clickSpace() {}
+function clickSpace(index) {
+  if (gameBoard[index] != '' || !spaceInUse || gameOver) {
+    return
+  }
+  updateSpace(index)
+  checkForWinner()
+  switchPlayer()
+}
 
-function updateSpace() {}
+function updateSpace(gameGrid) {
+  gameBoard[gameGrid] = currentPlayer
+  gameSpace[gameGrid].innerHTML = currentPlayer
+}
 
-function switchPlayer() {}
+function switchPlayer() {
+  if (currentPlayer === 1 && !gameOver) {
+    gameSpace[i].classList.add('space')
+    currentPlayer = 2
+    p1.innerHTML = currentPlayer
+    gameAlerts.innerHTML = `${currentPlayer}'s turn!`
+  } else if (currentPlayer === 2 && !gameOver) {
+    gameSpace[i].classList.add('space')
+    currentPlayer = 1
+    p2.innerHTML = currentPlayer
+    gameAlerts.innerHTML = `${currentPlayer}'s turn!`
+  }
+}
 
 function checkForWinner() {}
 
